c946388096f66369e16e54c31b09df391ed35bed
kingkyle <mackings@gmail.com>
2018-12-08 05:58:52 +0100

    added to update1

--- main/forms.py
+++ main/forms.py
@@ -5,4 +5,6 @@ from users.models import Profile
 class UserCreationProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['country']
+        fields = ['country', 'phone']
+        widgets = {'phone': forms.NumberInput(attrs={'aria-label': 'Sizing example input', 'aria-describedby': 'nputGroup-sizing-default',
+                                                     'class': 'form-control'})}
